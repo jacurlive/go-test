5fbd9e737840b2b9a4a91688c37ba9174f14f788
jacurlive <jasurkkirchh@gmail.com>
2024-08-24 21:42:57 +0500

    if else, switch case

--- test/test.go
+++ test/test.go
@@ -1,15 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
 
+// every programm start here (main function - is first step of every programm)
 func main() {
-	message, enter := enterTheClub(18)
-	fmt.Println(message, enter)
+	message, error := enterTheClub(18)
+	if error != nil {
+		log.Fatal(error)
+	}
+	fmt.Println(message)
+	prediction := prediction("пн")
+	fmt.Println(prediction)
 }
 
-func enterTheClub(age int) (string, bool) {
+// if else function
+func enterTheClub(age int) (string, error) {
 	if age >= 18 {
-		return "Welcome", true
+		return "Welcome", nil
 	}
-	return "STOP!", false
+	return "STOP!", errors.New("you are too young")
+}
+
+// switch case function
+func prediction(dayOfWeek string) string {
+	switch dayOfWeek {
+	case "пн":
+		return "Понедельник"
+	case "вт":
+		return "Вторник"
+	default:
+		return "No prediction"
+	}
+
 }
