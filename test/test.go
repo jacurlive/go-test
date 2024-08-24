7112d891e5cea133c0343a226598e506790ad372
jacurlive <jasurkkirchh@gmail.com>
2024-08-24 22:29:07 +0500

    for loop, unlimited arguments and first problem solving on leetcode

--- test/test.go
+++ test/test.go
@@ -15,6 +15,10 @@ func main() {
 	fmt.Println(message)
 	prediction := prediction("пн")
 	fmt.Println(prediction)
+	minNumber := findMin(12, 15, 17, 11, 34, 54, 9)
+	fmt.Println(minNumber)
+	result := twoSum([]int{3, 2, 4}, 14)
+	fmt.Println(result)
 }
 
 // if else function
@@ -36,4 +40,32 @@ func prediction(dayOfWeek string) string {
 		return "No prediction"
 	}
 
+}
+
+// for loop, unlimited number of arguments
+func findMin(numbers ...int) int {
+	if len(numbers) == 0 {
+		return 0
+	}
+
+	min := numbers[0]
+
+	for _, i := range numbers {
+		if min > i {
+			min = i
+		}
+	}
+	return min
+}
+
+// first leetcode problem https://leetcode.com/problems/two-sum/submissions/1366939120/
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nums
 }
